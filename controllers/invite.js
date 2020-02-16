c260fdbdc71c47ac8261b0f556f19494be79c19d
Khaos <khaos@khaos-coders.org>
2020-02-16 01:09:17 +0100

    Added link to start page

--- controllers/invite.js
+++ controllers/invite.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 // Define the invite page route.
-router.get('/', function(req, res) {
-  res.render('invite', null);
+router.get('/:token?', function(req, res) {
+    res.render('invite', null);
 });
 
 module.exports = router;
