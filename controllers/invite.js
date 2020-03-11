e4ec68f851cfa5b877150af50d5713d0f7649e01
Khaos <khaos@khaos-coders.org>
2020-03-11 21:01:44 +0100

    Fixed assignment of guest role for local users

--- controllers/invite.js
+++ controllers/invite.js
@@ -2,10 +2,7 @@ const debug = require('debug')('wedical:invite');
 const express = require('express');
 const router = express.Router();
 const csrf = require('csurf');
-const {
-    check,
-    validationResult
-} = require('express-validator');
+const { check, validationResult } = require('express-validator');
 const dataExt = require('../extension/data-ext');
 var Invite = require('../models/invite');
 var Guest = require('../models/guest');
@@ -22,18 +19,16 @@ var csrfProtection = csrf();
  * @param {any} value
  * @param {Object} param1
  */
-async function checkEmailExists(value, {
-    req
-}) {
-    value = value.trim().toLowerCase();
-    let user = await User.findOne({
-        email: value
-    });
-    // Unkown email, or email of same user
-    let valid = !user || user._id == req.params.id;
-    if (!valid) {
-        throw new Error('Email is already in use');
-    }
+async function checkEmailExists(value, { req }) {
+	value = value.trim().toLowerCase();
+	let user = await User.findOne({
+		email: value
+	});
+	// Unkown email, or email of same user
+	let valid = !user || user._id == req.params.id;
+	if (!valid) {
+		throw new Error('Email is already in use');
+	}
 }
 
 /**
@@ -41,388 +36,386 @@ async function checkEmailExists(value, {
  * @param {any} value
  * @param {Object} param1
  */
-async function checkTokenExists(value, {
-    req
-}) {
-    value = value.trim();
-    let invite = await Invite.findOne({
-        token: value
-    });
-    if (!invite) {
-        throw new Error('No invite with this token found');
-    }
+async function checkTokenExists(value, { req }) {
+	value = value.trim();
+	let invite = await Invite.findOne({
+		token: value
+	});
+	if (!invite) {
+		throw new Error('No invite with this token found');
+	}
 }
 
 async function findInviteGuest(req) {
-    if (!req.params.token || !req.params.uid) {
-        return null;
-    }
+	if (!req.params.token || !req.params.uid) {
+		return null;
+	}
 
-    let invite = await Invite.findOne({
-        token: req.params.token
-    });
-    if (!invite || invite.guests.indexOf(req.params.uid) < 0) {
-        return null;
-    }
+	let invite = await Invite.findOne({
+		token: req.params.token
+	});
+	if (!invite || invite.guests.indexOf(req.params.uid) < 0) {
+		return null;
+	}
 
-    var guest = await Guest.findOne({
-        _id: req.params.uid
-    });
-    if (!guest) {
-        return null;
-    }
+	var guest = await Guest.findOne({
+		_id: req.params.uid
+	});
+	if (!guest) {
+		return null;
+	}
 
-    return {
-        invite: invite,
-        guest: guest
-    };
+	return {
+		invite: invite,
+		guest: guest
+	};
 }
 
 // Define the invite code page route.
-router.get('/', csrfProtection, function (req, res) {
-    res.render('invite/code.pug', {
-        csrfToken: req.csrfToken(),
-        bodyClass: 'invite invite-code',
-    });
+router.get('/', csrfProtection, function(req, res) {
+	res.render('invite/code.pug', {
+		csrfToken: req.csrfToken(),
+		bodyClass: 'invite invite-code'
+	});
 });
 
-
 // Define the invite register page route.
-router.get('/:token/register/:utoken', csrfProtection, async function (req, res) {
-    if (!req.params.token || !req.params.utoken) {
-        return res.status(404).end();
-    }
+router.get('/:token/register/:utoken', csrfProtection, async function(req, res) {
+	if (!req.params.token || !req.params.utoken) {
+		return res.status(404).end();
+	}
 
-    let invite = await Invite.findOne({
-        token: req.params.token
-    });
-    if (!invite) {
-        return res.status(404).end();
-    }
+	let invite = await Invite.findOne({
+		token: req.params.token
+	});
+	if (!invite) {
+		return res.status(404).end();
+	}
 
-    var guest = await Guest.findOne({
-        token: req.params.utoken
-    });
-    if (!guest || invite.guests.indexOf(guest._id) < 0) {
-        return res.status(404).end();
-    }
+	var guest = await Guest.findOne({
+		token: req.params.utoken
+	});
+	if (!guest || invite.guests.indexOf(guest._id) < 0) {
+		return res.status(404).end();
+	}
 
-    // check if already registered
-    if (!guest.userId) {
-        req.session.guestid = guest._id;
-        // For redirect after social login
-        req.session.redirect_url = '/profile';
-    }
+	// check if already registered
+	if (!guest.userId) {
+		req.session.guestid = guest._id;
+		// For redirect after social login
+		req.session.redirect_url = '/profile';
+	}
 
-    res.render('invite/register.pug', {
-        csrfToken: req.csrfToken(),
-        guest: guest,
-        bodyClass: 'invite invite-code',
-    });
+	res.render('invite/register.pug', {
+		csrfToken: req.csrfToken(),
+		guest: guest,
+		bodyClass: 'invite invite-code'
+	});
 });
 
-
 // Define the invite register page route.
-router.post('/:token/register/:utoken',
-    csrfProtection,
-    check('password').isLength({
-        min: 5
-    }),
-    check('password2').custom((value, {
-        req
-    }) => value === req.body.password).withMessage('Passwords don\'t match'),
-    check('email').isEmail().bail().custom(checkEmailExists),
-    async function (req, res) {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(422).json({
-                errors: errors.array()
-            });
-        }
+router.post(
+	'/:token/register/:utoken',
+	csrfProtection,
+	check('password').isLength({
+		min: 5
+	}),
+	check('password2').custom((value, { req }) => value === req.body.password).withMessage("Passwords don't match"),
+	check('email').isEmail().bail().custom(checkEmailExists),
+	async function(req, res) {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(422).json({
+				errors: errors.array()
+			});
+		}
 
-        if (!req.session.guestid) {
-            return res.status(403).end();
-        }
+		if (!req.session.guestid) {
+			return res.status(403).end();
+		}
 
-        if (!req.params.token || !req.params.utoken) {
-            return res.status(404).end();
-        }
+		if (!req.params.token || !req.params.utoken) {
+			return res.status(404).end();
+		}
 
-        let invite = await Invite.findOne({
-            token: req.params.token
-        });
-        if (!invite) {
-            return res.status(404).end();
-        }
+		let invite = await Invite.findOne({
+			token: req.params.token
+		});
+		if (!invite) {
+			return res.status(404).end();
+		}
 
-        var guest = await Guest.findOne({
-            token: req.params.utoken
-        });
-        if (!guest || invite.guests.indexOf(guest._id) < 0) {
-            return res.status(404).end();
-        }
+		var guest = await Guest.findOne({
+			token: req.params.utoken
+		});
+		if (!guest || invite.guests.indexOf(guest._id) < 0) {
+			return res.status(404).end();
+		}
 
-        // Create new user
-        let user = await User.create({
-            guestId: guest._id,
-            name: guest.name,
-            email: req.body[`email`],
-            roles: [await Role.findOne({
-                name: 'Guest'
-            })]
-        });
+		// Create new user
+		let user = await User.create({
+			guestId: guest._id,
+			name: guest.name,
+			email: req.body[`email`],
+			roles: [
+				(await Role.findOne({
+					name: 'Guest'
+				}))._id
+			]
+		});
 
-        // set password
-        user.setLocalPw(req.body[`password`]);
-        await user.save();
+		// set password
+		user.setLocalPw(req.body[`password`]);
+		await user.save();
 
-        // Update email in guest
-        guest.email = req.body[`email`];
-        guest.userId = user._id;
-        await guest.save();
+		// Update email in guest
+		guest.email = req.body[`email`];
+		guest.userId = user._id;
+		await guest.save();
 
-        // Reset register session attributes
-        req.session.guestid = '';
-        req.session.redirect_url = '';
+		// Reset register session attributes
+		req.session.guestid = '';
+		req.session.redirect_url = '';
 
-        // login as the new user
-        req.login(user, function (err) {
-            if (err) {
-                return next(err);
-            }
-            return res.json({
-                redirect: '/profile'
-            });
-        });
-    });
-
+		// login as the new user
+		req.login(user, function(err) {
+			if (err) {
+				return next(err);
+			}
+			return res.json({
+				redirect: '/profile'
+			});
+		});
+	}
+);
 
 // Define the invite code form route.
-router.post('/',
-    csrfProtection,
-    check('token').notEmpty().bail().custom(checkTokenExists),
-    function (req, res) {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            debug(`invite form error: ${errors}`);
-            return res.redirect('/invite');
-        }
-        return res.redirect(`/invite/${req.body.token}`);
-    });
-
-// Define the invite page route.
-router.get('/:token', csrfProtection, async function (req, res) {
-    if (!req.params.token) {
-        return res.redirect('/invite');
-    }
-
-    let invite = await Invite.findOne({
-        token: req.params.token
-    });
-    if (!invite) {
-        return res.redirect('/invite');
-    }
-    if (!['open', 'accepted', 'declined'].indexOf(invite.state) < 0) {
-        return res.redirect('/invite');
-    }
-
-    let guests = await Guest.find({
-        _id: {
-            $in: invite.guests
-        }
-    });
-
-    let address = '';
-    for (let guest of guests) {
-        if (!address) {
-            address = guest.address;
-        }
-    }
-    let claimedTickets = dataExt.sum(guests.map(guest => guest.state == 'attending' ? 1 : 0));
-
-    res.render(`invite/${invite.state}.pug`, {
-        bodyClass: 'invite',
-        csrfToken: req.csrfToken(),
-        invite: invite,
-        address: address,
-        gmapsKey: config.gmaps.key,
-        claimedTickets: claimedTickets,
-        guests: guests.map(guest => invite.addInviteLink(guest)),
-    });
+router.post('/', csrfProtection, check('token').notEmpty().bail().custom(checkTokenExists), function(req, res) {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		debug(`invite form error: ${errors}`);
+		return res.redirect('/invite');
+	}
+	return res.redirect(`/invite/${req.body.token}`);
 });
 
+// Define the invite page route.
+router.get('/:token', csrfProtection, async function(req, res) {
+	if (!req.params.token) {
+		return res.redirect('/invite');
+	}
+
+	let invite = await Invite.findOne({
+		token: req.params.token
+	});
+	if (!invite) {
+		return res.redirect('/invite');
+	}
+	if (![ 'open', 'accepted', 'declined' ].indexOf(invite.state) < 0) {
+		return res.redirect('/invite');
+	}
+
+	let guests = await Guest.find({
+		_id: {
+			$in: invite.guests
+		}
+	});
+
+	let address = '';
+	for (let guest of guests) {
+		if (!address) {
+			address = guest.address;
+		}
+	}
+	let claimedTickets = dataExt.sum(guests.map((guest) => (guest.state == 'attending' ? 1 : 0)));
+
+	res.render(`invite/${invite.state}.pug`, {
+		bodyClass: 'invite',
+		csrfToken: req.csrfToken(),
+		invite: invite,
+		address: address,
+		gmapsKey: config.gmaps.key,
+		claimedTickets: claimedTickets,
+		guests: guests.map((guest) => invite.addInviteLink(guest))
+	});
+});
 
 // Define the decline invite route.
-router.get('/:token/decline', csrfProtection, async function (req, res) {
-    if (!req.params.token) {
-        return res.redirect('/invite');
-    }
+router.get('/:token/decline', csrfProtection, async function(req, res) {
+	if (!req.params.token) {
+		return res.redirect('/invite');
+	}
 
-    let invite = await Invite.findOne({
-        token: req.params.token
-    });
-    if (!invite) {
-        return res.redirect('/invite');
-    }
+	let invite = await Invite.findOne({
+		token: req.params.token
+	});
+	if (!invite) {
+		return res.redirect('/invite');
+	}
 
-    await invite.decline();
+	await invite.decline();
 
-    return res.redirect(`/invite/${req.params.token}`);
+	return res.redirect(`/invite/${req.params.token}`);
 });
 
 // Define the accept invite route.
-router.get('/:token/accept', csrfProtection, async function (req, res) {
-    if (!req.params.token) {
-        return res.redirect('/invite');
-    }
+router.get('/:token/accept', csrfProtection, async function(req, res) {
+	if (!req.params.token) {
+		return res.redirect('/invite');
+	}
 
-    let invite = await Invite.findOne({
-        token: req.params.token
-    });
-    if (!invite) {
-        return res.redirect('/invite');
-    }
+	let invite = await Invite.findOne({
+		token: req.params.token
+	});
+	if (!invite) {
+		return res.redirect('/invite');
+	}
 
-    await invite.accept();
+	await invite.accept();
 
-    return res.redirect(`/invite/${req.params.token}`);
+	return res.redirect(`/invite/${req.params.token}`);
 });
 
 // Define the change guest state route.
-router.post('/:token/gstate/:uid', csrfProtection, async function (req, res) {
-    let result = await findInviteGuest(req);
-    if (!result || req.body.value === undefined) {
-        return res.status(404).end();
-    }
+router.post('/:token/gstate/:uid', csrfProtection, async function(req, res) {
+	let result = await findInviteGuest(req);
+	if (!result || req.body.value === undefined) {
+		return res.status(404).end();
+	}
 
-    if (req.body.value === 'true') {
-        result.guest.state = 'attending';
-    } else {
-        result.guest.state = 'absent';
-    }
+	if (req.body.value === 'true') {
+		result.guest.state = 'attending';
+	} else {
+		result.guest.state = 'absent';
+	}
 
-    await result.guest.save();
+	await result.guest.save();
 
-    return res.status(200).end('ok');
+	return res.status(200).end('ok');
 });
 
 // Define the get guest diet/allergies route.
-router.get('/:token/gdiet/:uid', csrfProtection, async function (req, res) {
-    let result = await findInviteGuest(req);
-    if (!result) {
-        return res.status(404).end();
-    }
+router.get('/:token/gdiet/:uid', csrfProtection, async function(req, res) {
+	let result = await findInviteGuest(req);
+	if (!result) {
+		return res.status(404).end();
+	}
 
-    var pojo = result.guest.toPOJO();
-    pojo.allergy = pojo.allergy || [];
-    pojo.diet = pojo.diet || [];
+	var pojo = result.guest.toPOJO();
+	pojo.allergy = pojo.allergy || [];
+	pojo.diet = pojo.diet || [];
 
-    res.setHeader('Content-Type', 'application/json');
-    return res.end(JSON.stringify({
-        data: {
-            allergy: pojo.allergy,
-            diet: pojo.diet
-        }
-    }));
+	res.setHeader('Content-Type', 'application/json');
+	return res.end(
+		JSON.stringify({
+			data: {
+				allergy: pojo.allergy,
+				diet: pojo.diet
+			}
+		})
+	);
 });
 
 // Define the put guest diet/allergies route.
-router.put('/:token/gdiet/:uid', csrfProtection, async function (req, res) {
-    let result = await findInviteGuest(req);
-    if (!result || req.body.allergy1 === undefined || req.body.diet1 === undefined) {
-        return res.status(404).end();
-    }
+router.put('/:token/gdiet/:uid', csrfProtection, async function(req, res) {
+	let result = await findInviteGuest(req);
+	if (!result || req.body.allergy1 === undefined || req.body.diet1 === undefined) {
+		return res.status(404).end();
+	}
 
-    result.guest.allergy = [];
-    result.guest.diet = [];
+	result.guest.allergy = [];
+	result.guest.diet = [];
 
-    for (var field in req.body) {
-        if (typeof (req.body[field]) !== 'string' || !req.body[field]) {
-            continue;
-        }
-        if (field.startsWith('allergy')) {
-            result.guest.allergy.push(req.body[field]);
-        } else if (field.startsWith('diet')) {
-            result.guest.diet.push(req.body[field]);
-        }
-    }
+	for (var field in req.body) {
+		if (typeof req.body[field] !== 'string' || !req.body[field]) {
+			continue;
+		}
+		if (field.startsWith('allergy')) {
+			result.guest.allergy.push(req.body[field]);
+		} else if (field.startsWith('diet')) {
+			result.guest.diet.push(req.body[field]);
+		}
+	}
 
-    await result.guest.save();
+	await result.guest.save();
 
-    return res.status(200).end('ok');
+	return res.status(200).end('ok');
 });
 
 // Define the get guest invite link route.
-router.get('/:token/ginvite/:uid', csrfProtection, async function (req, res) {
-    let result = await findInviteGuest(req);
-    if (!result) {
-        return res.status(404).end();
-    }
+router.get('/:token/ginvite/:uid', csrfProtection, async function(req, res) {
+	let result = await findInviteGuest(req);
+	if (!result) {
+		return res.status(404).end();
+	}
 
-    result.invite.addInviteLink(result.guest);
+	result.invite.addInviteLink(result.guest);
 
-    res.setHeader('Content-Type', 'application/json');
-    return res.end(JSON.stringify({
-        data: {
-            name: result.guest.name,
-            inviteLink: result.guest.inviteLink
-        }
-    }));
+	res.setHeader('Content-Type', 'application/json');
+	return res.end(
+		JSON.stringify({
+			data: {
+				name: result.guest.name,
+				inviteLink: result.guest.inviteLink
+			}
+		})
+	);
 });
 
 // Define the get guest invite link route.
-router.post('/:token/claim',
-    csrfProtection,
-    check('name').notEmpty(),
-    check('email').isEmail().bail().custom(checkEmailExists),
-    async function (req, res) {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(422).json({
-                errors: errors.array()
-            });
-        }
+router.post(
+	'/:token/claim',
+	csrfProtection,
+	check('name').notEmpty(),
+	check('email').isEmail().bail().custom(checkEmailExists),
+	async function(req, res) {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(422).json({
+				errors: errors.array()
+			});
+		}
 
-        if (!req.params.token) {
-            return res.status(404).end('Not found');
-        }
+		if (!req.params.token) {
+			return res.status(404).end('Not found');
+		}
 
-        let invite = await Invite.findOne({
-            token: req.params.token
-        });
-        if (!invite) {
-            return res.status(404).end('Not found');
-        }
+		let invite = await Invite.findOne({
+			token: req.params.token
+		});
+		if (!invite) {
+			return res.status(404).end('Not found');
+		}
 
-        // Check tickets
-        let guests = await Guest.find({
-            _id: {
-                $in: invite.guests
-            }
-        });
+		// Check tickets
+		let guests = await Guest.find({
+			_id: {
+				$in: invite.guests
+			}
+		});
 
-        let claimedTickets = dataExt.sum(guests.map(guest => guest.state == 'attending' ? 1 : 0));
-        if (claimedTickets >= parseInt(invite.tickets)) {
-            return res.status(403).end('All Tickets gone');
-        }
+		let claimedTickets = dataExt.sum(guests.map((guest) => (guest.state == 'attending' ? 1 : 0)));
+		if (claimedTickets >= parseInt(invite.tickets)) {
+			return res.status(403).end('All Tickets gone');
+		}
 
-        let guest = await Guest.create({
-            name: req.body.name,
-            email: req.body.email,
-            group: invite.title
-        });
+		let guest = await Guest.create({
+			name: req.body.name,
+			email: req.body.email,
+			group: invite.title
+		});
 
-        guest.setAttending();
-        await guest.save();
+		guest.setAttending();
+		await guest.save();
 
-        invite.guests.push(guest._id);
-        await invite.save();
+		invite.guests.push(guest._id);
+		await invite.save();
 
-        invite.addInviteLink(guest);
-        return res.json({
-            redirect: guest.inviteLink
-        });
-    });
-
+		invite.addInviteLink(guest);
+		return res.json({
+			redirect: guest.inviteLink
+		});
+	}
+);
 
 module.exports = router;
