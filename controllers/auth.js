b078dd196a32edea230bc99af31f969195333f54
Khaos <khaos@khaos-coders.org>
2020-03-11 21:19:46 +0100

    Auth changed

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,49 +4,52 @@ const passport = require('passport');
 var User = require('../models/user');
 var Guest = require('../models/guest');
 
-router.get('/success',
-    passport.authenticate('session'),
-    async function (req, res) {
-        if (!req.user) {
-            return res.status(403).end('Authentication failed');
-        }
+const handleLogin = async function (req, res) {
+    if (!req.user) {
+        return res.status(403).end('Authentication failed');
+    }
 
-        // Find user
-        let user = await User.findOne({
-            _id: req.user.id
-        });
-        if (!user) {
-            return res.status(404).end('Not found');
-        }
-
-        // Check if invited. Else delete.
-        let guestid = req.session.guestid;
-        req.session.guestid = '';
-        let redirect_url = req.session.redirect_url || '/profile';
-        req.session.redirect_url = '';
-
-        if (!guestid) {
-            if (!user.guestId) {
-                req.logout();
-                await user.remove();
-                return res.status(403).end('You don\'t seem to be invited!');
-            }
-        } else {
-            // Assign guestId
-            user.guestId = guestid;
-            await user.save();
-
-            // assign email
-            let guest = await Guest.findOne({
-                _id: guestid
-            });
-            guest.email = user.email;
-            guest.userId = user._id;
-            await guest.save();
-        }
-
-        return res.redirect(redirect_url);
+    // Find user
+    let user = await User.findOne({
+        _id: req.user.id
     });
+    if (!user) {
+        return res.status(404).end('Not found');
+    }
+
+    // Check if invited. Else delete.
+    let guestid = req.session.guestid;
+    req.session.guestid = '';
+    let redirect_url = req.session.redirect_url || '/profile';
+    req.session.redirect_url = '';
+
+    if (!guestid) {
+        if (!user.guestId) {
+            req.logout();
+            await user.remove();
+
+            req.flash('error', {
+                param: 'Auth',
+                msg: 'You don\'t seem to be invited'
+            });
+            return res.redirect('/login?redirect_url=' + encodeURIComponent(redirect_url));
+        }
+    } else {
+        // Assign guestId
+        user.guestId = guestid;
+        await user.save();
+
+        // assign email
+        let guest = await Guest.findOne({
+            _id: guestid
+        });
+        guest.email = user.email;
+        guest.userId = user._id;
+        await guest.save();
+    }
+
+    return res.redirect(redirect_url);
+};
 
 // Google
 router.get('/google',
@@ -58,9 +61,7 @@ router.get('/google/callback',
     passport.authenticate('google', {
         failureRedirect: '/login'
     }),
-    function (req, res) {
-        res.redirect('/auth2/success');
-    });
+    handleLogin);
 
 // Facebook
 router.get('/facebook', passport.authenticate('facebook', {
@@ -71,8 +72,6 @@ router.get('/facebook/callback',
     passport.authenticate('facebook', {
         failureRedirect: '/login'
     }),
-    function (req, res) {
-        res.redirect('/auth2/success');
-    });
+    handleLogin);
 
 module.exports = router;
